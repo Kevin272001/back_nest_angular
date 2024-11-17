3075c727686d98e28e539351b03d726975e13fdd
Kevin272001 <kevin27carvajalg@gmail.com>
2024-11-17 00:39:56 -0500

    segundt

--- src/modules/categoria/categoria.provider.ts
+++ src/modules/categoria/categoria.provider.ts
@@ -1,11 +1,12 @@
 import { DataSource } from "typeorm";
 import { Categoria } from "./entities/categoria.entity";
 
+
 export const categoriaProvider = [
     {
         provide: 'CATEGORIA_REPOSITORY',
         useFactory: (dataSource:DataSource)=>dataSource.getRepository(Categoria),
-        inject: ['DATABASE_CONNECTION']
+        inject: ['DATABASE_CONNECTION_POSTGRES']
     }
 
  ]
